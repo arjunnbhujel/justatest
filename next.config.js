3b65e9970d26553b870206cdc6395f0e771d4d82
Arjun Bhujel <arjunbhujel600@gmail.com>
2024-03-29 17:05:55 +0545

    2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withMDX = require("@next/mdx")()
 
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
